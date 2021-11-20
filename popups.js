02eb3d8ed0b158bfc0489b344a5ee9dc7a49ee28
biromiro <n.r.t.c@hotmail.com>
2021-11-20 13:46:14 +0000

    Add Sign In component

--- popups.js
+++ popups.js
@@ -27,7 +27,6 @@ backgroundRules.addEventListener('click', () => {
     rules.classList.toggle('active');
 });
 
-
 const ldbButton = document.getElementsByClassName('ldb-button')[0]
 const backgroundLdb = document.getElementsByClassName('background-ldb')[0]
 const closeLdb = document.getElementsByClassName('close-ldb')[0]
@@ -48,4 +47,26 @@ closeLdb.addEventListener('click', () => {
 backgroundLdb.addEventListener('click', () => {
     ldbContainer.classList.toggle('active')
     ldb.classList.toggle('active')
+});
+
+const signinButton = document.getElementsByClassName('signin-button')[0]
+const backgroundSignin = document.getElementsByClassName('background-signin')[0]
+const closeSignin = document.getElementsByClassName('close-signin')[0]
+const signinContainer = document.getElementsByClassName('signin-container')[0]
+const signin = document.getElementsByClassName('signin')[0]
+
+signinButton.addEventListener('click', () => {
+    signinContainer.classList.toggle('active')
+    signin.classList.toggle('active')
+});
+
+closeSignin.addEventListener('click', () => {
+    signinContainer.classList.toggle('active')
+    signin.classList.toggle('active')
+});
+
+
+backgroundSignin.addEventListener('click', () => {
+    signinContainer.classList.toggle('active')
+    signin.classList.toggle('active')
 });
