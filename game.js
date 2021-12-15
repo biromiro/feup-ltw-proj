4a8996330281ee47b1d568b96f7b8ad58e486899
Joao Baltazar <joaoferreirabaltazar@gmail.com>
2021-12-15 21:07:21 +0000

    add sow functionality

--- game.js
+++ game.js
@@ -1,19 +1,5 @@
 class Board
 {
-    genDisplay()
-    {
-        this.element = document.createElement('div');
-        this.element.className = 'board';
-        this.body.appendChild(this.element);
-
-        const root = document.querySelector(':root');
-        root.style.setProperty('--nCavities', this.nCavities);
-
-        this.cavities.forEach(cavity => {
-            cavity.genDisplay();
-        });
-    }
-
     constructor(body, nCavities, nSeeds)
     {
         this.body = body;
@@ -32,6 +18,65 @@ class Board
         for (let i = 0; i < this.nCavities; i++)
             this.cavities.push(new Cavity(this, nSeeds));
     }
+
+    genDisplay()
+    {
+        this.element = document.createElement('div');
+        this.element.className = 'board';
+        this.body.appendChild(this.element);
+
+        const root = document.querySelector(':root');
+        root.style.setProperty('--nCavities', this.nCavities);
+
+        this.cavities.forEach(cavity => {
+            cavity.genDisplay();
+        });
+    }
+
+    updateDisplay()
+    {
+        this.cavities.forEach(cavity => {
+            cavity.updateDisplay();
+        });
+    }
+
+    sow(cavityIdx)
+    {
+        let sourceCavity = this.cavities[cavityIdx];
+
+        if(sourceCavity.specifier)
+        {
+            alert('oopsie!');
+            return;
+        }
+
+        if(!sourceCavity.seeds.length)
+        {
+            alert('oopsie daisy!');
+            return;
+        }
+
+        const sowableCavities = [...this.cavities];
+
+        for (let i = 0; i < sowableCavities.length; i++) {
+            const cavity = sowableCavities[i];
+            if(cavity.specifier && cavity.player() != sourceCavity.player()) {
+                sowableCavities.pop(cavity);
+                break;
+            }
+        }
+
+        for (const [i, seed] of sourceCavity.seeds.entries())
+        {
+            const targetCavity = sowableCavities[cavityIdx + i + 1];
+
+            seed.cavity = targetCavity;
+            
+            targetCavity.seeds.push(seed);
+        }
+
+        sourceCavity.seeds = [];
+    }
 }
 
 class Cavity
@@ -45,6 +90,31 @@ class Cavity
         this.seeds.forEach(seed => {
             seed.genDisplay();
         });
+    }
+
+    updateDisplay()
+    {
+        this.deleteChildren();
+
+        this.seeds.forEach(cavity => {
+            cavity.genDisplay();
+        });
+    }
+
+    player()
+    {
+        const idx = this.board.cavities.indexOf(this);
+        return idx < this.board.nCavities + 1 ? 1 : 2;
+    }
+
+    deleteChildren()
+    {
+        let child = this.element.lastElementChild; 
+        
+        while (child) {
+            this.element.removeChild(child);
+            child = this.element.lastElementChild;
+        }
     }
 
     constructor(board, nSeeds, specifier)
@@ -69,14 +139,18 @@ class Seed
     {
         const width = this.cavity.element.offsetWidth;
         const height = this.cavity.element.offsetHeight;
+
+        const proportionsElement = this.cavity.board.element.querySelector('.cavity:not(.c-big)');
+        const xProportion = proportionsElement.offsetWidth;
+        const yProportion = proportionsElement.offsetHeight;
         
         const xOffset = width / 8;
         const yOffset = height / 8;
         const x = Math.random() * width*1.5 + xOffset;
         const y = Math.random() * height*1.5 + yOffset;
         const degs = Math.random() * 360;
-        const scaleX = 0.005 * width;
-        const scaleY = 0.006 * height;
+        const scaleX = 0.005 * xProportion;
+        const scaleY = 0.006 * yProportion;
 
         this.element = document.createElement('div');
         this.element.className = 'seed';
